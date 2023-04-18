db54db1893efca43753f0f78720bbcb8601cf3c3
konradkn <konradkn@outlook.com>
2023-04-18 15:45:56 -0600

    correct links

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ let summarySelector = () => {
         document.getElementById('summary').innerHTML = 'Show more +'
     }
 }
+
 document.getElementById('summary').addEventListener('click', summarySelector)
 
 let projectSelector = document.querySelectorAll('.project');
